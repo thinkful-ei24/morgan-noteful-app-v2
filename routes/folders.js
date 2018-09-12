c93daf15669430c5858035d66c94911ae380f6f1
Morgan Newman <mpnewman95@gmail.com>
2018-09-12 12:43:43 -0700

    Added /api/folders/:id DELETE endpoint support

--- routes/folders.js
+++ routes/folders.js
@@ -30,7 +30,6 @@ router.get('/:id', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const name = req.body.name;
-
   // Validate name field (required)
   if (!name) {
     const err = new Error('Missing `name` in request body.');
@@ -44,10 +43,46 @@ router.post('/', (req, res, next) => {
     .returning(['id', 'name'])
     .then(dbResponse => {
       if (!dbResponse.length) return next();
-      else return res.status(201).json(dbResponse);
+      else return res.status(201).json(dbResponse[0]);
     })
     .catch(err => next(err));
+});
 
+router.put('/:id', (req, res, next) => {
+  const name = req.body.name;
+  const id = req.params.id;
+  // Validate name field (required)
+  if (!name) {
+    const err = new Error('Missing `name` in request body.');
+    err.status = 400;
+    next(err);
+  }
+
+  knex('folders')
+    .update({
+      name
+    })
+    .where('id', id)
+    .returning(['id', 'name'])
+    .then(dbResponse => {
+      if (!dbResponse.length) return next();
+      else return res.status(200).json(dbResponse[0]);
+    })
+    .catch(err => next(err));
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  knex
+    .delete()
+    .from('folders')
+    .where('id', id)
+    .then(dbResponse => {
+      if (dbResponse === 0) return next();
+      else res.status(204).end();
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
