91961d01bdfaaa9e178bdc54180bf8b32c0380de
Morgan Newman <mpnewman95@gmail.com>
2018-09-12 12:03:07 -0700

    Router working

--- routes/folders.js
+++ routes/folders.js
@@ -4,3 +4,8 @@ const knex = require('../knex');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.status(200).json('hello!');
+});
+
+module.exports = router;
