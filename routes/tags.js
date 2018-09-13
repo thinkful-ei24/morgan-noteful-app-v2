ee5e5832c40f8af209788a7f1acaf685aef5ee9f
Morgan Newman <mpnewman95@gmail.com>
2018-09-13 09:05:36 -0700

    Added skeleton for tagsRouter

---
+++ routes/tags.js
@@ -0,0 +1,6 @@
+const knex = require('../knex');
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { PORT } = require('./config');
 
 const notesRouter = require('./routes/notes');
 const foldersRouter = require('./routes/folders');
+const tagsRouter = require('./routes/tags');
 
 // Create an Express application
 const app = express();
@@ -27,6 +28,7 @@ app.use(express.json());
 // Mount router on "/api"
 app.use('/api/notes', notesRouter);
 app.use('/api/folders', foldersRouter);
+app.use('/api/tags', tagsRouter);
 
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
