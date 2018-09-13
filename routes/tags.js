342dedf355f4962e84f2e3a29b9407e7954517ba
Morgan Newman <mpnewman95@gmail.com>
2018-09-13 09:12:33 -0700

    Fleshed out skeleton for all endpoints and added support for /api/tags/ GET endpoint

--- routes/tags.js
+++ routes/tags.js
@@ -2,5 +2,22 @@ const knex = require('../knex');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  knex
+    .select(['id', 'name'])
+    .from('tags')
+    .then((dbResponse) => {
+      res.status(200).json(dbResponse);
+    })
+    .catch(err => next(err));
+});
+
+router.get('/:id', (req, res, next) => {});
+
+router.post('/', (req, res, next) => {});
+
+router.put('/:id', (req, res, next) => {});
+
+router.delete('/:id', (req, res, next) => {});
 
 module.exports = router;
